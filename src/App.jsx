86ab75e6ec275b04ebf452b6cbe67efa57229657
Eddie Jacobian <edjacobian@gmail.com>
2023-08-01 14:22:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div id="container">
+        <h1></h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+      </div>
+     </>
+  )
+}
+
+export default App
